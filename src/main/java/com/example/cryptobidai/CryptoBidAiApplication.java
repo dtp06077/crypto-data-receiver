804d1157024d4b47786fd5852cb9ff16b11886aa
dtp06077 <dtp06077@gmail.com>
2024-11-19 15:29:29 +0900

    INIT COMMIT

---
+++ src/main/java/com/example/cryptobidai/CryptoBidAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptobidai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoBidAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoBidAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cryptobidai/CryptoBidAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cryptobidai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoBidAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
