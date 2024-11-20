62ecb1cc8c8e9e6f3d911c0e6e27941b8b1bf265
dtp06077 <dtp06077@gmail.com>
2024-11-20 14:43:45 +0900

    FEAT: WebSocketConfig 추가

--- src/main/java/com/example/cryptobidai/websocket/config/WebSocketConfig.java
+++ src/main/java/com/example/cryptobidai/websocket/config/WebSocketConfig.java
@@ -1,15 +1,47 @@
 package com.example.cryptobidai.websocket.config;
 
+import com.example.cryptobidai.constant.BhConstant;
+import com.example.cryptobidai.websocket.handler.CryptoWebSocketHandler;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.client.WebSocketClient;
+import org.springframework.web.socket.client.WebSocketConnectionManager;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-public class WebSocketConfig {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final CryptoWebSocketHandler cryptoWebSocketHandler;
+
+    public WebSocketConfig(CryptoWebSocketHandler cryptoWebSocketHandler) {
+        this.cryptoWebSocketHandler = cryptoWebSocketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(cryptoWebSocketHandler, "/client")
+                .setAllowedOrigins("*"); //임시로 모든 출처에서의 연결 허용
+    }
 
     @Bean
     public WebSocketClient webSocketClient() {
         return new StandardWebSocketClient();
+    }
+
+    // WebSocket 연결을 위한 메서드
+    @Bean
+    public WebSocketConnectionManager connectionManager() {
+        WebSocketConnectionManager connectionManager = new WebSocketConnectionManager(
+                webSocketClient(),
+                cryptoWebSocketHandler,
+                BhConstant.WS_PUBLIC_URL // 빗썸 url
+        );
+        connectionManager.start(); // 연결 시작
+        return connectionManager;
     }
 }
--- src/main/java/com/example/cryptobidai/websocket/handler/CryptoWebSocketHandler.java
+++ src/main/java/com/example/cryptobidai/websocket/handler/CryptoWebSocketHandler.java
@@ -1,16 +1,13 @@
 package com.example.cryptobidai.websocket.handler;
 
-import com.example.cryptobidai.constant.BhConstant;
+
 import com.example.cryptobidai.transfer.JsonRequestTransfer;
-import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.client.WebSocketClient;
-import org.springframework.web.socket.client.WebSocketConnectionManager;
 import org.springframework.web.socket.handler.BinaryWebSocketHandler;
 
 import java.util.ArrayList;
@@ -20,42 +17,27 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CryptoWebSocketHandler extends BinaryWebSocketHandler {
 
-    private final WebSocketClient webSocketClient;
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) {
+        List<String> codesList = new ArrayList<>();
+        codesList.add("KRW-BTC");
+        codesList.add("KRW-ETH.3");
+        String message = JsonRequestTransfer.orderBookRequestTransfer("test example", codesList, 10);
+        System.out.println(message);
+        try {
+            session.sendMessage(new TextMessage(message)); // 요청 메시지를 전송
+        } catch (Exception e) {
+            System.out.println("error: " + e.getMessage());
+        }
+    }
 
-    @PostConstruct
-    public void connectToWebSocket() {
+    @Override
+    protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) {
+        System.out.println("Received binary message: " + message.getPayload());
+    }
 
-        String url = BhConstant.WS_PUBLIC_URL;
-
-        BinaryWebSocketHandler binaryWebSocketHandler = new BinaryWebSocketHandler() {
-
-            @Override
-            public void afterConnectionEstablished(WebSocketSession session) {
-                List<String> codesList = new ArrayList<>();
-                codesList.add("KRW-BTC");
-                codesList.add("KRW-ETH.3");
-                String message = JsonRequestTransfer.orderBookRequestTransfer("test example", codesList, 10);
-                System.out.println(message);
-                try {
-                    session.sendMessage(new TextMessage(message));
-                } catch (Exception e) {
-                    System.out.println("error: " + e.getMessage());
-                }
-
-            }
-
-            @Override
-            protected void handleBinaryMessage(WebSocketSession session, BinaryMessage message) {
-                System.out.println("Received binary message: " + message.getPayload());
-            }
-
-            @Override
-            public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
-                System.out.println("WebSocket connection closed: " + status);
-            }
-        };
-
-        WebSocketConnectionManager connectionManager = new WebSocketConnectionManager(webSocketClient, binaryWebSocketHandler, url);
-        connectionManager.start();
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        System.out.println("WebSocket connection closed: " + status);
     }
 }
