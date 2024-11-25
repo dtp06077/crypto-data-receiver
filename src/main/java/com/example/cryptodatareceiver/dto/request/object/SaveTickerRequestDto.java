18ee7e180f2c8225d6a01201e35be057034e991b
dtp06077 <dtp06077@gmail.com>
2024-11-25 16:15:12 +0900

    FEAT: Jackson 라이브러리 추가 및 SaveTickerRequestDto 변경

--- src/main/java/com/example/cryptodatareceiver/dto/request/object/SaveTickerRequestDto.java
+++ src/main/java/com/example/cryptodatareceiver/dto/request/object/SaveTickerRequestDto.java
@@ -1,5 +1,6 @@
 package com.example.cryptodatareceiver.dto.request.object;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,17 +8,42 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SaveTickerRequestDto {
 
+    @JsonProperty("opening_price")
     private double openingPrice;
+
+    @JsonProperty("high_price")
     private double highPrice;
+
+    @JsonProperty("low_price")
     private double lowPrice;
+
+    @JsonProperty("trade_price")
     private double tradePrice;
+
+    @JsonProperty("prev_closing_price")
     private double prevClosingPrice;
+
+    @JsonProperty("highest_52_week_price")
     private double highest52WeekPrice;
+
+    @JsonProperty("lowest_52_week_price")
     private double lowest52WeekPrice;
+
+    @JsonProperty("trade_volume")
     private double tradeVolume;
+
+    @JsonProperty("acc_trade_volume")
     private double accTradeVolume;
+
+    @JsonProperty("acc_trade_price")
     private double accTradePrice;
+
+    @JsonProperty("trade_date")
     private String tradeDate;
+
+    @JsonProperty("trade_time")
     private String tradeTime;
+
+    @JsonProperty("trade_timestamp")
     private long tradeTimeStamp;
 }
---
+++ src/main/java/com/example/cryptodatareceiver/transfer/JsonToRequestTransfer.java
@@ -0,0 +1,7 @@
+package com.example.cryptodatareceiver.transfer;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class JsonToRequestTransfer {
+}
