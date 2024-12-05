c7b65257f092fc8c1ff7c8779987983d33150d77
dtp06077 <dtp06077@gmail.com>
2024-12-05 15:16:24 +0900

    REFACTOR: buffer_time을 producer에서 관리하도록 수정

--- src/main/java/com/example/cryptodatareceiver/rest/executor/TickerExecutor.java
+++ src/main/java/com/example/cryptodatareceiver/rest/executor/TickerExecutor.java
@@ -3,8 +3,12 @@ package com.example.cryptodatareceiver.rest.executor;
 import com.example.cryptodatareceiver.kafka.KafkaProducerService;
 import com.example.cryptodatareceiver.rest.service.TickerService;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONObject;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 
 @Component
@@ -18,6 +22,19 @@ public class TickerExecutor {
     @Scheduled(fixedRate = 60000)
     public void tickerProcess() {
         String textMessage = tickerService.getTickerRequest("KRW-BTC");
+
+        //bufferTime을 producer에서 관리하도록 수정
+        String bufferTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+
+        textMessage = addBufferTimeToJson(textMessage, bufferTime);
+
         kafkaProducerService.sendToProducer("crypto_topic_dev", textMessage);
+    }
+
+    private String addBufferTimeToJson(String json, String bufferTime) {
+        JSONObject jsonObject = new JSONObject(json);
+        jsonObject.put("buffer_time", bufferTime);
+
+        return jsonObject.toString();
     }
 }
